cc40463b9cc55094132ba2354a13ff2d7adb8d3d
Roger Tuan <r.tuan@datocms.com>
2025-02-27 02:09:03 -0800

    Copyediting

--- src/app/(base-layout)/hotel-details/[slug]/page.tsx
+++ src/app/(base-layout)/hotel-details/[slug]/page.tsx
@@ -252,15 +252,15 @@ export default async function Page({ params }: { params: { slug: string } }) {
                     <svg
                       stroke="currentColor"
                       fill="currentColor"
-                      strokeWidth="0"
+                      stroke-width="0"
                       viewBox="0 0 24 24"
-                      className="me-2 tw-inline"
+                      className="me-2"
                       height="20px"
                       width="20px"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path fill="none" d="M0 0h24v24H0z"></path>
-                      <path d="M12 12c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm6-1.8C18 6.57 15.35 4 12 4s-6 2.57-6 6.2c0 2.34 1.95 5.44 6 9.14 4.05-3.7 6-6.8 6-9.14zM12 2c4.2 0 8 3.22 8 8.2 0 3.32-2.67 7.25-8 11.8-5.33-4.55-8-8.48-8-11.8C4 5.22 7.8 2 12 2z"></path>
+                      <path fill="none" d="M0 0h24v24H0V0z"></path>
+                      <path d="M6.54 5c.06.89.21 1.76.45 2.59l-1.2 1.2c-.41-1.2-.67-2.47-.76-3.79h1.51m9.86 12.02c.85.24 1.72.39 2.6.45v1.49c-1.32-.09-2.59-.35-3.8-.75l1.2-1.19M7.5 3H4c-.55 0-1 .45-1 1 0 9.39 7.61 17 17 17 .55 0 1-.45 1-1v-3.49c0-.55-.45-1-1-1-1.24 0-2.45-.2-3.57-.57a.84.84 0 0 0-.31-.05c-.26 0-.51.1-.71.29l-2.2 2.2a15.149 15.149 0 0 1-6.59-6.59l2.2-2.2c.28-.28.36-.67.25-1.02A11.36 11.36 0 0 1 8.5 4c0-.55-.45-1-1-1z"></path>
                     </svg>
                     <p className="body-text small my-0 align-self-end">
                       <a target="_blank" href={`tel:${hotel.phone}`}>
@@ -385,7 +385,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
           {hotel.customAmenities && (
             <>
               <h2 className="header medium my-0 pb-3 tw-scroll-mt-12" id="amenities">
-                Custom Amenities
+                Special Features
               </h2>
               <div className="flex-wrap hstack gap-2">
                 <div className="body-text regular my-0">
@@ -393,7 +393,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
                     {hotel.customAmenities.map((amenity) => (
                       <li>
                         {amenity.name}
-                        {!!amenity.description && ` - ${amenity.description}`}
+                        {!!amenity.description && `: ${amenity.description}`}
                       </li>
                     ))}
                   </ul>
@@ -531,7 +531,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
           <div className="vstack gap-3">
             {hotel.faq && (
               <>
-                <h2>FAQs for {hotel.name}</h2>
+                <h2>Frequently Asked Questions (FAQs) for {hotel.name}</h2>
                 <StructuredText
                   data={hotel.faq}
                   renderInlineRecord={({ record }) => (
@@ -546,7 +546,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
             {hotel.brand && (
               <>
-                <h2>FAQs for {hotel.brand.brandName}</h2>
+                <h2>More FAQs for {hotel.brand.brandName}</h2>
                 <StructuredText data={hotel.brand.brandFaqs} />
               </>
             )}
